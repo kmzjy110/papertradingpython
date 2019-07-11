0ac4a567bba9d62c62af0c78ef6e203b63e4f6ca
harry zhao <kmzwg@icloud.com>
2019-07-11 16:23:14 -0400

    ols_pairs_trading more

--- experiments.py
+++ experiments.py
@@ -8,6 +8,6 @@ symbol_pair = 'AAPL'
 now = consts.api.get_clock().timestamp
 print(now)
 end_dt = now - pd.Timedelta(str(lookback) + ' days') + pd.Timedelta(str(20) + ' days')
-prices = helper.get_prices(symbol_pair, end_dt, lookback=lookback)
+prices = helper.get_prices(symbol_pair, end_dt, lookback=lookback).loc[:,'AAPL']
 print(prices)
 print(helper.current_prices('AAPL'))
--- ols_pairs_trading.py
+++ ols_pairs_trading.py
@@ -10,17 +10,39 @@ def get_target_portfolio_weights():
 
 def get_positions(symbol_pairs, lookback = consts.lookback):
     for x,y in symbol_pairs:
-        x_close = helper.prices_up_to_yesterday(x,lookback).loc[:, x].loc[:, 'close']
-        y_close = helper.prices_up_to_yesterday(y,lookback).loc[:, y].loc[:, 'close']
+        current_prices = helper.current_prices(symbol_pairs)
+        x_current= current_prices.loc[:, x].loc[:, 'close']
+        y_current = current_prices.loc[:, y].loc[:, 'close']
         try:
-            hedge = get_hedge_ratio(y_close,x_close)
+            hedge = get_hedge_ratio(y_current,x_current)
         except ValueError as e:
             logging.error(e)
             return
         target_weights = helper.get_current_portfolio_weights()
-        spreads = get_spreads((x,y), hedge)
+        spreads = get_spreads((x,y), hedge, lookback)
         zscore = (spreads[-1] - spreads.mean()) / spreads.std()
-        
+
+        if (consts.inShort.get(symbol_pairs) and zscore<0.0 )or (consts.inLong.get(symbol_pairs) and zscore >0.0):
+            target_weights.loc[x]=0
+            target_weights.loc[y] = 0
+            logging.log("Exiting positions of "+ symbol_pairs[0] + " and "+ symbol_pairs[1])
+            consts.inLong[symbol_pairs]=False
+            consts.inShort[symbol_pairs] = False
+            return target_weights
+        if not consts.inLong.get(symbol_pairs) and zscore < -1:
+            y_target_shares = 1
+            x_target_shares = -hedge
+            consts.inLong[symbol_pairs] = True
+            consts.inShort[symbol_pairs] = False
+        if not consts.inShort.get(symbol_pairs) and zscore >1:
+            y_target_shares = -1
+            x_target_shares = hedge
+            consts.inLong[symbol_pairs] = False
+            consts.inShort[symbol_pairs] = True
+        x_target_pct, y_target_pct = get_holding_percentage(x_target_shares,y_target_shares,x_current,y_current)
+        target_weights.loc[x] = x_target_pct * (1.0/len(symbol_pairs))
+        target_weights.loc[y] = y_target_pct * (1.0/len(symbol_pairs))
+        return target_weights
     pass
 
 def get_spreads(symbol_pair, hedge, lookback = consts.lookback):
@@ -50,3 +72,9 @@ def get_spreads(symbol_pair, hedge, lookback = consts.lookback):
 def get_hedge_ratio(Y,X,add_const=True):
 
     pass
+
+def get_holding_percentage(xShares, yShares, xPrice, yPrice):
+    xdol = xShares * xPrice;
+    ydol = yShares * yPrice;
+    total = abs(xdol) + abs(ydol)
+    return (xdol/total), (ydol/total)
