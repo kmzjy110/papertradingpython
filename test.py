24e872a4a6b74012922c513dd3384c30b5ebf2b5
Harry Zhao <harry.zhao@berkeley.edu>
2019-07-10 23:06:03 -0400

    get prices done

--- test.py
+++ test.py
@@ -32,5 +32,18 @@ def prices(symbols):
         end_dt = now - pd.Timedelta(now.strftime('%H:%M:%S')) - pd.Timedelta('1 minute')
     return _get_prices(symbols,end_dt)
 
-def _get_prices(symbols, end_dt, max_workers =5):
+def _get_prices(symbols, end_dt, max_workers =5, lookback=20):
+    start_dt = end_dt - pd.Timedelta(lookback+' days')
+    start = start_dt.strftime('%Y-%-m-%-d')
+    end = end_dt.strftime('%Y-%-m-%-d')
+
+    barset = None
+    i=0
+    while i<=len(symbols)-1:
+        if barset is None:
+            barset = api.get_barset(symbols[i:i+200])
+        else:
+            barset.update(api.get_barset(symbols[i:i+200]))
+        i+=200
+    return barset.df
 
